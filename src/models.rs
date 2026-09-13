c55f7199024af8be1d46843dd2441dda4dc30a30
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-13 20:23:12 +0200

    Separate threaded comment projection

--- src/models.rs
+++ src/models.rs
@@ -88,11 +88,21 @@ pub struct CreatePost {
 pub struct Comment {
     pub id: Uuid,
     pub post_id: Uuid,
-    #[sqlx(default)]
+    pub author_id: Uuid,
+    pub body: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+    pub version: i64,
+}
+
+#[derive(Debug, Serialize, FromRow)]
+#[serde(rename_all = "camelCase")]
+pub struct ThreadedComment {
+    pub id: Uuid,
+    pub post_id: Uuid,
     pub parent_comment_id: Option<Uuid>,
     pub author_id: Uuid,
     pub body: String,
-    #[sqlx(default)]
     pub deleted_at: Option<DateTime<Utc>>,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
