7e4f9c28980c8376f623cd40395adc010cac7122
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-13 20:19:24 +0200

    Model threaded comment relationships

--- src/models.rs
+++ src/models.rs
@@ -88,8 +88,12 @@ pub struct CreatePost {
 pub struct Comment {
     pub id: Uuid,
     pub post_id: Uuid,
+    #[sqlx(default)]
+    pub parent_comment_id: Option<Uuid>,
     pub author_id: Uuid,
     pub body: String,
+    #[sqlx(default)]
+    pub deleted_at: Option<DateTime<Utc>>,
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
     pub version: i64,
@@ -97,6 +101,11 @@ pub struct Comment {
 
 #[derive(Debug, Deserialize)]
 pub struct CreateComment {
+    pub body: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateReply {
     pub body: String,
 }
 
